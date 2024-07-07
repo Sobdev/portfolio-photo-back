aa078e10f3c06bfc243875e570d40ec77e46bf23
Sobdev <71671270+Sobdev@users.noreply.github.com>
2024-07-07 13:29:40 +0200

    Photo models

---
+++ models/Photo.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const photoSchema = new Schema({
+    title: String,
+    description: String,
+    url: String,
+    date: { type: Date, default: Date.now }
+});
+
+const Photo = mongoose.model('Photo', photoSchema)
+module.exports = Photo
---
+++ routes/photos.routes.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const Photo = require('../models/Photo.model')
+
+router.get('/', async (req, res) => {
+    try {
+        const photos = await Photo.find();
+        res.json(photos);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.post('/', async (req, res) => {
+    const photo = new Photo({
+        title: req.body.title,
+        description: req.body.description,
+        url: req.body.url
+    });
+
+    try {
+        const newPhoto = await photo.save();
+        res.status(201).json(newPhoto);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+router.get('/:id', getPhoto, (req, res) => {
+    res.json(res.photo);
+});
+
+async function getPhoto(req, res, next) {
+    let photo;
+    try {
+        photo = await Photo.findById(req.params.id);
+        if (photo == null) {
+            return res.status(404).json({ message: 'Photo not found' });
+        }
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+
+    res.photo = photo;
+    next();
+}
+
+module.exports = router;
